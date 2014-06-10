e62f5b1a980551bd66cde9b1196b1face2640946
Igor Afanasyev <afan@evernote.com>
2014-06-10 19:49:11 +0400

    Add social buttons in the footer
    Change-Id: Id5b7e572c82cecd56e58815193d32c95e0728fb4
    Reviewed-on: https://gerrit.corp.etonreve.com/158268
    Reviewed-by: Igor Afanasyev <afan@evernote.com>
    Tested-by: Igor Afanasyev <afan@evernote.com>

--- inc/footer.php
+++ inc/footer.php
@@ -2,8 +2,32 @@
         </div><!-- /content -->
 
         <div class="footer">
+        	<ul class="social-buttons">
+        		<li><iframe src="http://ghbtns.com/github-btn.html?user=evernote&amp;repo=serge&amp;type=watch&amp;count=true" allowtransparency="true" frameborder="0" scrolling="0" width="110" height="20"></iframe></li>
+        		<li><div class="fb-like" data-href="https://serge.io/" data-width="120px" data-height="20px" data-layout="button_count" data-action="like"></div></li>
+        		<li><a href="https://twitter.com/share" class="twitter-share-button" data-url="http://serge.io/" data-dnt="true"></a></li>
+        	</ul>
         	Serge and Serge website are &copy; 2014 Evernote Corporation. All rights reserved.<br/>Serge is licensed under GNU General Public License.
         </div>
     </div><!-- /wrapper -->
+
+    <!-- Facebook -->
+    <script>(function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+    <!-- /Facebook -->
+
+    <!-- Twitter -->
+    <script>
+        !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';
+        if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';
+        fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+    </script>
+    <!-- /Twitter -->
+
 </body>
 </html>
