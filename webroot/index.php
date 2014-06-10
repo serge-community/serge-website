e42f712d2a3dae6016b58a60ba7ee1286af6bb68
Igor Afanasyev <afan@evernote.com>
2014-06-10 20:40:14 +0400

    Typo
    Change-Id: I615f5a61868b8c3e90c2b4c889c812e4ae4220b6
    Reviewed-on: https://gerrit.corp.etonreve.com/158284
    Reviewed-by: Igor Afanasyev <afan@evernote.com>
    Tested-by: Igor Afanasyev <afan@evernote.com>

--- webroot/index.php
+++ webroot/index.php
@@ -6,7 +6,7 @@
 <div class="graphic-bg"></div>
 <div class="graphic"></div>
 
-<p><strong>Serge</strong> (it's name stands for <u>S</u>tring <u>E</u>xtraction and <u>R</u>esource <u>G</u>eneration <u>E</u>ngine) <strong>is a free, open-source toolkit</strong> that helps you set up a continous localization process for your various pieces of software and marketing materials in a fully automated and scalable fashion. It will fit perfectly into your agile development cycle.</p>
+<p><strong>Serge</strong> (it's name stands for <u>S</u>tring <u>E</u>xtraction and <u>R</u>esource <u>G</u>eneration <u>E</u>ngine) <strong>is a free, open-source toolkit</strong> that helps you set up a continuous localization process for your various pieces of software and marketing materials in a fully automated and scalable fashion. It will fit perfectly into your agile development cycle.</p>
 
 <p>Serge is developed and maintained by <a href="http://evernote.com/">Evernote</a>, where it works non-stop to help deliver dozens of different Evernote clients, website and marketing assets in 30+ languages.</p>
 
