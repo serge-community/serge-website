39b8dadbe8f1d99131a77170ef4735f5800cc248
Igor Afanasyev <afan@evernote.com>
2014-06-11 13:15:03 +0400

    Disable social buttons while website is still in active development
    Change-Id: Ib4e659faeb5db91da9d7917b58f903185927b6c9
    Reviewed-on: https://gerrit.corp.etonreve.com/158556
    Reviewed-by: Igor Afanasyev <afan@evernote.com>
    Tested-by: Igor Afanasyev <afan@evernote.com>

--- inc/footer.php
+++ inc/footer.php
@@ -2,15 +2,18 @@
         </div><!-- /content -->
 
         <div class="footer">
+            <?php /*
         	<ul class="social-buttons">
         		<li><iframe src="http://ghbtns.com/github-btn.html?user=evernote&amp;repo=serge&amp;type=watch&amp;count=true" allowtransparency="true" frameborder="0" scrolling="0" width="110" height="20"></iframe></li>
         		<li><div class="fb-like" data-href="https://serge.io/" data-width="120px" data-height="20px" data-layout="button_count" data-action="like"></div></li>
         		<li><a href="https://twitter.com/share" class="twitter-share-button" data-url="http://serge.io/" data-dnt="true"></a></li>
         	</ul>
+            */ ?>
         	Serge and Serge website are &copy; 2014 Evernote Corporation. All rights reserved.<br/>Serge is licensed under GNU General Public License.
         </div>
     </div><!-- /wrapper -->
 
+    <?php /*
     <!-- Facebook -->
     <script>(function(d, s, id) {
         var js, fjs = d.getElementsByTagName(s)[0];
@@ -28,6 +31,7 @@
         fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
     </script>
     <!-- /Twitter -->
+    */ ?>
 
 </body>
 </html>
