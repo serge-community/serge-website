005b76f95f28b191bc410d63507059034bc879c5
Igor Afanasyev <afan@evernote.com>
2015-05-22 14:27:53 -0700

    Typo
    Change-Id: I81ffb223d7afc38e7786488b719dc36b1bcb802f
    Reviewed-on: https://gerrit.ba1.etonreve.com/261876
    Reviewed-by: Igor Afanasyev <afan@evernote.com>
    Tested-by: Igor Afanasyev <afan@evernote.com>

--- webroot/documentation/continuous-localization/index.php
+++ webroot/documentation/continuous-localization/index.php
@@ -10,6 +10,6 @@
 
 <p>Continuous localization revolutionizes this process much like continous build integration revolutionized software development process. <strong>It is a way of automatically, seamlessly gathering new source material, publishing it for translation, acquiring translations and integrating them back into the product.</strong> Ultimately, it makes manual localization management — exporting, converting files, sending them for translation, doing backward conversion, committing changes to version control — unnecessary.</p>
 
-<p>Imagine that developers only ever need to deal with one source language, and — magically! — see all localized resources being updated in their version control system by a tireless localization robot as soon as translations arrive. This is excatly what Serge helps to achieve. Building robust multi-platform multilingual products in agile development environment suddenly becomes a manageable goal.</p>
+<p>Imagine that developers only ever need to deal with one source language, and — magically! — see all localized resources being updated in their version control system by a tireless localization robot as soon as translations arrive. This is exactly what Serge helps to achieve. Building robust multi-platform multilingual products in agile development environment suddenly becomes a manageable goal.</p>
 
 <?php include($_SERVER['DOCUMENT_ROOT'] . '/../inc/documentation-footer.php') ?>
