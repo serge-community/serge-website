c8b9bd987b4954af022b37c65c602e90d502e309
Igor Afanasyev <afan@evernote.com>
2015-06-01 12:33:32 -0700

    Serge.io: plugin documentation improvements
    Change-Id: If3e9c3a5736b3d9ccfad2580d153724bbe3ecbeb
    Reviewed-on: https://gerrit.ba1.etonreve.com/263610
    Reviewed-by: Igor Afanasyev <afan@evernote.com>
    Tested-by: Igor Afanasyev <afan@evernote.com>

--- webroot/docs/plugins/parse_json/index.php
+++ webroot/docs/plugins/parse_json/index.php
@@ -7,7 +7,118 @@
 
 <h1><?php echo htmlspecialchars($title) ?></h1>
 
-<p>...</p>
+<p>This plugin is used to parse arbitrary JSON objects. It uses regular expressions as a configuration paramater to match translatable nodes in the JSON object tree.</p>
+
+<p>If your JSON is a plain dictionary in <code>"key" : "value"</code> format, where all keys need to be translated, use the <a href="/docs/plugins/parse_json_keyvalue">parse_json_keyvalue</a> plugin instead.</p>
+
+<p>In case JSON format validation fails, the plugin can send an error report to specified recipients. If no email settings provided, it will simply report the error in the console output.</p>
+
+<h2>Code Examples</h2>
+
+<figure>
+    <figcaption>products.json</figcaption>
+    <code class="block">{
+    "description" => "Product list",
+    "<span class="hint">products</span>": [
+        {
+            "sku": "P001",
+            "price": 1.23,
+            "<span class="hint">title</span>": "<span class="string">First Product</span>"
+            "<span class="hint">description</span>": "<span class="string">First Product Description</span>"
+        },
+        {
+            "sku": "P002",
+            "price": 2.34,
+            "<span class="hint">title</span>": "<span class="string">Second Product</span>"
+            "<span class="hint">description</span>": "<span class="string">Second Product Description</span>"
+        },
+        //...
+    ],
+    //...
+}
+</code>
+</figure>
+
+<p>Please see the example configuration file below to learn why only <code>title</code> and certain <code>description</code> nodes are extracted here for translation.</p>
+
+<h2>Node Paths</h2>
+
+<p>When JSON document is parsed, each node in it is given its path, and this path is what <code>path_matches</code> and <code>path_doesnt_match</code> <a href="http://www.regular-expressions.info/">regular expressions</a> should match against (see the example configuration file). This is how the paths are constructed, given the example JSON file above:</p>
+
+<figure>
+    <figcaption>products.json (internal 'path => value' representation)</figcaption>
+    <code class="block"><span class="nop" >description</span>             => <span class="nop"   >Product list</span>
+<span class="nop" >products[0]/sku</span>         => <span class="nop"   >P001</span>
+<span class="nop" >products[0]/price</span>       => <span class="nop"   >1.23</span>
+<span class="hint">products[0]/title</span>       => <span class="string">First Product</span>
+<span class="hint">products[0]/description</span> => <span class="string">First Product Description</span>
+<span class="nop" >products[1]/sku</span>         => <span class="nop"   >P001</span>
+<span class="nop" >products[1]/price</span>       => <span class="nop"   >1.23</span>
+<span class="hint">products[1]/title</span>       => <span class="string">Second Product</span>
+<span class="hint">products[1]/description</span> => <span class="string">Second Product Description</span>
+//...
+</code>
+</figure>
+
+<p>The constructed path to a node is also extracted as a hint along with the corresponding string.</p>
+
+<h2>Usage</h2>
+
+<figure>
+    <figcaption>example-project.serge</figcaption>
+    <script language="text/x-config-neat">
+jobs
+{
+    :sample-job
+    {
+        plugin                   parse_json
+
+        data
+        {
+            /*
+              one or more regular expressions to match the node
+              path against; if any of the regular expressions
+              match, the node value is extracted for translation.
+
+              Note that '/', '[', and ']' symbols need to be
+              escaped in a regular expression
+
+              In the example below, we want to extract all
+              `title` and `description` terminal nodes
+              no matter where they are located in the object tree
+            */
+            path_matches         \/(title|description)$
+
+            /*
+              optional: one ore more regular expressions to match
+              the node path against; if any of the regular
+              expressions match, the node value is NOT extracted
+              for translation
+
+              In the example below, we want to skip (prevent
+              from being translated) the top-level `description`
+              terminal node
+            */
+            path_doesnt_match    ^description$
+
+            # email to send error reports on behalf of
+            email_from           l10n-robot@acme.org
+
+            # one or more email addresses
+            # to send error reports to
+            email_to             engineer@acme.org
+                                 project-manager@acme.org
+
+            # email subject
+            email_subject        Errors found in JSON file
+        }
+
+        # other job parameters
+        # ...
+    }
+}
+</script>
+</figure>
 
 <?php include($_SERVER['DOCUMENT_ROOT'] . '/../inc/documentation-footer.php') ?>
 
--- webroot/docs/plugins/parse_json_keyvalue/index.php
+++ webroot/docs/plugins/parse_json_keyvalue/index.php
@@ -1,13 +1,62 @@
 <?php
     $section = 'parser-plugins';
     $subpage = 'ref-plugin-parse_json_keyvalue';
-    $title = 'JSON Key-Value Array Parser Plugin';
+    $title = 'JSON Dictionary Parser Plugin';
     include($_SERVER['DOCUMENT_ROOT'] . '/../inc/documentation-header.php');
 ?>
 
 <h1><?php echo htmlspecialchars($title) ?></h1>
 
-<p>...</p>
+<p>This plugin is used to parse plain JSON objects (dictionaries) in <code>"key" : "value"</code> format.</p>
+
+<p>Note that this parser does full JSON parsing/validation, and thus can be used on JSON only. If you have dictionaries in arbitrary JavaScript file, use the <a href="/docs/plugins/parse_js">parse_js</a> plugin instead. If your JSON has an arbitrary structure, see the <a href="/docs/plugins/parse_json">parse_json</a> plugin that gives more flexibility on what nodes to translate.</p>
+
+<p>In case JSON format validation fails, the plugin can send an error report to specified recipients. If no email settings provided, it will simply report the error in the console output.</p>
+
+<h2>Code Examples</h2>
+
+<figure>
+    <figcaption>example.json</figcaption>
+    <code class="block">{
+    "<span class="hint">key1</span>": "<span class="string">string</span>",
+    "<span class="hint">key2</span>": "<span class="string">string</span>",
+    "<span class="hint">key3</span>": "<span class="string">string</span>",
+    //...
+}
+</code>
+</figure>
+
+<h2>Usage</h2>
+
+<figure>
+    <figcaption>example-project.serge</figcaption>
+    <script language="text/x-config-neat">
+jobs
+{
+    :sample-job
+    {
+        plugin               parse_json_keyvalue
+
+        data
+        {
+            # email to send error reports on behalf of
+            email_from       l10n-robot@acme.org
+
+            # one or more email addresses
+            # to send error reports to
+            email_to         engineer@acme.org
+                             project-manager@acme.org
+
+            # email subject
+            email_subject    Errors found in JSON file
+        }
+
+        # other job parameters
+        # ...
+    }
+}
+</script>
+</figure>
 
 <?php include($_SERVER['DOCUMENT_ROOT'] . '/../inc/documentation-footer.php') ?>
 
