a0a71dffbf34416a0c615d982713b899451867aa
Igor Afanasyev <afan@evernote.com>
2015-06-04 16:36:11 -0700

    Serge.io: plugin documentation improvements
    Change-Id: I583a044f242ee63d0d292dd6b0de3e6dc87d6ce7
    Reviewed-on: https://gerrit.ba1.etonreve.com/264691
    Reviewed-by: Igor Afanasyev <afan@evernote.com>
    Tested-by: Igor Afanasyev <afan@evernote.com>

--- webroot/docs/plugins/parse_strings/index.php
+++ webroot/docs/plugins/parse_strings/index.php
@@ -1,7 +1,7 @@
 <?php
     $section = 'parser-plugins';
     $subpage = 'ref-plugin-parse_strings';
-    $title = 'Mac OS .strings Parser Plugin';
+    $title = 'MacOS/iOS .strings Parser Plugin';
     include($_SERVER['DOCUMENT_ROOT'] . '/../inc/documentation-header.php');
 ?>
 
@@ -9,6 +9,59 @@
 
 <p>Plugin source location: <code>&lt;serge_root&gt;/lib/Serge/Engine/Plugin/parse_strings.pm</code></p>
 
-<p>...</p>
+<p>This parser extracts strings from Apple MacOS/iOS <a href="https://developer.apple.com/library/ios/documentation/Cocoa/Conceptual/LoadingResources/Strings/Strings.html">string resources</a>.
+
+<p>This plugin supports extracting one-line <code>/* ... */</code> style comments that go immediately above the key-value line, as well as <code>// ...</code> style comments at the end of the key-value line. The comment above, the string key, and the extra comment at the end of the string are combined as a hint.</p>
+
+<p>Also, plugin allows to specify the string context by appending <code>##<em>context</em></code> at the end of the key name (see the example below).</p>
+
+<p class="notice">Limitation: multi-line comments and key values are not supported yet. Also, <code>\U<em>XXXX</em></code> encoding for Unicode symbols is not supported. It is possible to use final Unicode symbols, though.</p>
+
+<h2>Code Examples</h2>
+
+<figure>
+    <figcaption>Localizable.strings</figcaption>
+    <code class="block">/* this comment is not attached to a string */
+
+/* <span class="hint">comment</span> */
+"<span class="hint">some key</span>" = "<span class="string">string</span>"
+
+/* <span class="hint">comment</span> */
+<span class="hint">some_key</span> = "<span class="string">string</span>" // <span class="hint">extra comment</span>
+
+/* this comment is not attached to a string */
+
+/* <span class="hint">comment</span> */
+"<span class="hint">some_key</span>##<span class="context">context</span>" = "<span class="string">string</span>" // <span class="hint">extra comment</span>
+
+...
+</code>
+</figure>
+
+<h2>Usage</h2>
+
+<figure>
+    <figcaption>example-project.serge</figcaption>
+    <script language="text/x-config-neat">
+jobs
+{
+    :sample-job
+    {
+        plugin               parse_strings
+
+        /*
+          .strings always use UTF-16LE output encoding,
+          and byte-order-mark (BOM) needs to be present
+          in the output file
+        */
+        output_encoding      UTF-16LE
+        output_bom           YES
+
+        # other job parameters
+        # ...
+    }
+}
+</script>
+</figure>
 
 <?php include($_SERVER['DOCUMENT_ROOT'] . '/../inc/documentation-footer.php') ?>
--- webroot/docs/plugins/parse_ts/index.php
+++ webroot/docs/plugins/parse_ts/index.php
@@ -1,7 +1,7 @@
 <?php
     $section = 'parser-plugins';
     $subpage = 'ref-plugin-parse_ts';
-    $title = 'QT .ts Parser Plugin';
+    $title = 'Qt Linguist TS Parser Plugin';
     include($_SERVER['DOCUMENT_ROOT'] . '/../inc/documentation-header.php');
 ?>
 
@@ -9,6 +9,47 @@
 
 <p>Plugin source location: <code>&lt;serge_root&gt;/lib/Serge/Engine/Plugin/parse_ts.pm</code></p>
 
-<p>...</p>
+<p>This parser extracts strings from <a href="http://doc.qt.io/qt-4.8/linguist-ts-file-format.html">Qt Linguist .TS files</a> (used in e.g. <a href="http://developer.blackberry.com/native/documentation/device_platform/internationalization/localization.html">BlackBerry 10</a> platform localization). Upon saving the destination file, it also adjusts the language in the top <code>&lt;TS&gt;</code> tag <code>language</code> attribute, and removes <code>type="unfinished"</code> attribute from individual <code>&lt;translation&gt;</code> attributes.</p>
+
+<p class="notice">Limitation: native <code>&lt;comment&gt;</code>, <code>&lt;extracomment&gt;</code>, and <code>&lt;translatorcomment&gt;</code> tag contents are not extracted as hints yet. Context names are also not extracted at the moment.</p>
+
+<h2>Code Examples</h2>
+
+<figure>
+    <figcaption>example.ts</figcaption>
+    <code class="block">&lt;TS version="2.0" language="<em>en_US</em>"&gt;
+    &lt;context&gt;
+        &lt;name&gt;main&lt;/name&gt;
+        &lt;message&gt;
+            &lt;source&gt;<span class="string">string</span>&lt;/source&gt;
+            &lt;translation <em>type="unfinished"</em>&gt;&lt;/translation&gt;
+        &lt;/message&gt;
+        &lt;message&gt;
+            &lt;source&gt;<span class="string">string</span>&lt;/source&gt;
+            &lt;translation <em>type="unfinished"</em>&gt;&lt;/translation&gt;
+        &lt;/message&gt;
+        ...
+    &lt;/context&gt;
+&lt;/TS&gt;
+</code>
+</figure>
+
+<h2>Usage</h2>
+
+<figure>
+    <figcaption>example-project.serge</figcaption>
+    <script language="text/x-config-neat">
+jobs
+{
+    :sample-job
+    {
+        plugin               parse_ts
+
+        # other job parameters
+        # ...
+    }
+}
+</script>
+</figure>
 
 <?php include($_SERVER['DOCUMENT_ROOT'] . '/../inc/documentation-footer.php') ?>
