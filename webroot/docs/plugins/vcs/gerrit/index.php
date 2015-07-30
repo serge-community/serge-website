1431a92bbdc642e6aa81ca30bef02c111687ae33
Igor Afanasyev <afan@evernote.com>
2015-07-30 10:46:34 -0700

    Serge.io: fix a typo
    Change-Id: I13687a9403d267a499f89ea7eb241b52d5097623
    Reviewed-on: https://gerrit.ba1.etonreve.com/277414
    Reviewed-by: Igor Afanasyev <afan@evernote.com>
    Tested-by: Igor Afanasyev <afan@evernote.com>

--- webroot/docs/plugins/vcs/gerrit/index.php
+++ webroot/docs/plugins/vcs/gerrit/index.php
@@ -9,7 +9,7 @@
 
 <p>Plugin source location: <code>&lt;serge_root&gt;/lib/Serge/Sync/Plugin/VCS/gerrit.pm</code></p>
 
-<p>This plugin provides integration with <a href="https://www.gerritcodereview.com/">Gerrit</a>-based source code repositories. Gerrit is a code review system built on top of Git, and it has a different way of submitting changes to the remote server (no direct `git push`). This plugin inherits its core behavior and settings from the <a href="/docs/plugins/vcs/git/">git plugin</a>, and automatically self-reviews the commiited change in Gerrit.</p>
+<p>This plugin provides integration with <a href="https://www.gerritcodereview.com/">Gerrit</a>-based source code repositories. Gerrit is a code review system built on top of Git, and it has a different way of submitting changes to the remote server (no direct `git push`). This plugin inherits its core behavior and settings from the <a href="/docs/plugins/vcs/git/">git plugin</a>, and automatically self-reviews the committed change in Gerrit.</p>
 
 <h2>Usage</h2>
 
