3805d194f839a056f66e2c1bcd3705a14827c77b
Igor Afanasyev <afan@evernote.com>
2015-12-04 12:15:31 -0800

    Update CLI documenation from Serge

--- webroot/docs/help/serge-test-parser/index.php
+++ webroot/docs/help/serge-test-parser/index.php
@@ -10,7 +10,7 @@
 
 <h1 id="SYNOPSIS">SYNOPSIS</h1>
 
-<p><code>serge test-parser &lt;parser_name&gt; &lt;file_path&gt; [--output-mode=&lt;mode&gt;]</code></p>
+<p><code>serge test-parser &lt;parser_name&gt; &lt;file_path&gt; [--import-mode] [--output-mode=&lt;mode&gt;]</code></p>
 
 <p>Where <code>&lt;parser_name&gt;</code> is a file name of the parser sans its extension (parsers are typically located in lib/Serge/Engine/Plugin folder), and <code>&lt;file_path&gt;</code> is a path to the localizable file to test this parser on.</p>
 
@@ -22,6 +22,12 @@
 
 <dl>
 
+<dt><b>--import-mode</b></dt>
+<dd>
+
+<p>With this option, parser will be told it works in import mode. In such mode import-aware parsers are expected to extract translations rather than source strings, and also skip missing translations.</p>
+
+</dd>
 <dt><b>--output-mode=mode</b></dt>
 <dd>
 
