2838a1a4b3690ab7976420b6f5d56c78b493ddd3
Igor Afanasyev <afan@evernote.com>
2016-06-18 17:30:05 +0300

    Add 403 erro handler page

---
+++ webroot/403.php
@@ -0,0 +1,8 @@
+<?php
+    $page = '403';
+    include($_SERVER['DOCUMENT_ROOT'] . '/../inc/header.php');
+?>
+
+<h1 class="error">Access denied</h1>
+
+<?php include($_SERVER['DOCUMENT_ROOT'] . '/../inc/footer.php') ?>
