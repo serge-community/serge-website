fd871a83432bc4af656c41df546b1fd6c6b6d991
Igor Afanasyev <afan@evernote.com>
2018-01-31 12:39:28 -0800

    Update documentation on .TS parser to match the new parser behavior

--- webroot/docs/plugins/parser/parse_ts/v/1.2/index.php
+++ webroot/docs/plugins/parser/parse_ts/v/1.2/index.php
@@ -11,19 +11,17 @@
 
 <p>Plugin source location: <code>&lt;serge_root&gt;/lib/Serge/Engine/Plugin/parse_ts.pm</code></p>
 
-<p>This parser extracts strings from <a href="http://doc.qt.io/qt-4.8/linguist-ts-file-format.html">Qt Linguist .TS files</a>. Upon saving the destination file, it also adjusts the language in the top <code>&lt;TS&gt;</code> tag <code>language</code> attribute, and removes <code>type="unfinished"</code> attribute from individual <code>&lt;translation&gt;</code> attributes.</p>
+<p>This parser extracts strings from <a href="http://doc.qt.io/qt-4.8/linguist-ts-file-format.html">Qt Linguist .TS files</a> (used in e.g. <a href="http://developer.blackberry.com/native/documentation/device_platform/internationalization/localization.html">BlackBerry 10</a> platform localization). Upon saving the destination file, it also adjusts the language in the top <code>&lt;TS&gt;</code> tag <code>language</code> attribute, and removes <code>type="unfinished"</code> attribute from individual <code>&lt;translation&gt;</code> attributes.</p>
 
 <h2>Code Examples</h2>
 
 <figure>
     <figcaption>example.ts</figcaption>
-    <code class="block">&lt;TS version="4.0" language="<em>en_US</em>"&gt;
+    <code class="block">&lt;TS version="2.0" language="<em>en_US</em>"&gt;
     &lt;context&gt;
-        &lt;name&gt;<span class="context">main</span>&lt;/name&gt;
+        &lt;name&gt;main&lt;/name&gt;
         &lt;message&gt;
             &lt;source&gt;<span class="string">string</span>&lt;/source&gt;
-            &lt;comment&gt;<span class="context">context</span>&lt;/comment&gt;
-            &lt;extracomment&gt;<span class="hint">comment</span>&lt;/extracomment&gt;
             &lt;translation <em>type="unfinished"</em>&gt;&lt;/translation&gt;
         &lt;/message&gt;
         &lt;message&gt;
@@ -36,7 +34,7 @@
 </code>
 </figure>
 
-<p class="notice">Keep in mind that <code>&lt;comment&gt;</code> tags in this file format actually hold the <em>context</em>, whereas developer <em>comments</em> must be provided in <code>&lt;extracomment&gt;</code> tags.</p>
+<p class="notice">Limitation: native <code>&lt;comment&gt;</code>, <code>&lt;extracomment&gt;</code>, and <code>&lt;translatorcomment&gt;</code> tag contents are not extracted as hints yet. Context names are also not extracted at the moment.</p>
 
 <h2>Usage</h2>
 
