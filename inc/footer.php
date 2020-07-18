e9708fc3dafd666c43ae3340b2a29d219e03e447
Igor Afanasyev <afan@evernote.com>
2020-07-17 20:41:30 -0700

    Remove social buttons and commented-out code

--- inc/footer.php
+++ inc/footer.php
@@ -10,35 +10,8 @@
                 Serge is licensed under Standard Perl License (dual <a href="http://dev.perl.org/licenses/gpl1.html">GPL</a> / <a href="http://dev.perl.org/licenses/artistic.html">Artistic License</a>)<br/>
                 Serge.io website and Serge documentation are licensed under <a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a>
             </div>
-            <ul class="social-buttons">
-                <li><iframe src="https://ghbtns.com/github-btn.html?user=evernote&amp;repo=serge&amp;type=watch&amp;count=true" allowtransparency="true" frameborder="0" scrolling="0" width="110" height="20"></iframe></li>
-                <?php /*
-                <li><div class="fb-like" data-href="https://serge.io/" data-width="120px" data-height="20px" data-layout="button_count" data-action="like"></div></li>
-                <li><a href="https://twitter.com/share" class="twitter-share-button" data-url="http://serge.io/" data-dnt="true"></a></li>
-                */ ?>
-            </ul>
         </div>
     </div><!-- /wrapper -->
-
-    <?php /*
-    <!-- Facebook -->
-    <script>(function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));</script>
-    <!-- /Facebook -->
-
-    <!-- Twitter -->
-    <script>
-        !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';
-        if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';
-        fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
-    </script>
-    <!-- /Twitter -->
-    */ ?>
 
     <script src="/media/vendor/anchorjs/anchor.min.js"></script>
     <script>
